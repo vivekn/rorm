37cc8c105eb73509ccfd757dde6b3b18b1f5f4b1
Vivek Narayanan <mail@vivekn.co.cc>
2011-07-04 07:35:27 +0530

    Fixed error due to inconsistency in redis-py library

--- rorm/__init__.py
+++ rorm/__init__.py
@@ -10,7 +10,6 @@ class Meta(type):
         cls._sets = []
         cls._lists = []
         cls._zsets = []
-
         for k,v in ns.items():
             if isinstance(v, Field):
                 if v.data_type == 'set':
@@ -21,10 +20,9 @@ class Meta(type):
                     cls._hashes.append(k)
                 elif v.data_type == 'zset':
                     cls._zsets.append(k)
-                del ns[k]
-                
+                delattr(cls, k)
 
-        cls._ctr = get_redis.get('%s.counter' % cls.__name__)
+        cls._ctr = get_redis().get('%s.counter' % cls.__name__)
 
     def create(cls):
         cls._ctr = get_redis().incr('%s.counter' % cls.__name__)
@@ -38,11 +36,11 @@ class SortedSet(object):
     def remove(self, field):
         get_redis().zrem(self.key, field)
 
-    def push(self, field, score)
-        get_redis().zincrby(self.key, score, field)
+    def push(self, field, score):
+        get_redis().zincrby(self.key, field, score)
 
-    def pull(self, limit = 10)
-        get_redis().zrevrange(self.key, 0, limit - 1)
+    def pull(self, limit = 10):
+        return get_redis().zrevrange(self.key, 0, limit - 1) or []
 
 
 
@@ -52,27 +50,32 @@ class Model(object):
         self.id = prefix
 
     def __getattr__(self, key):
-        _key = "%s.%s" % (self.prefix, key)
-        if key in self._sets:
-            return get_set(_key)
-        elif key in self._lists:
-            return get_list(_key)
-        elif key in self._zsets:
-            return get_list(_key)
-        elif key in self._hashes:
-            return get_hash(_key)
-        return _key
+        if key in ['prefix', 'id']:
+            return super(Model, self).__getattribute__(key)
+        else:
+            _key = "%s.%s" % (self.prefix, key)
+            if key in self._sets:
+                return get_set(_key)
+            elif key in self._lists:
+                return get_list(_key)
+            elif key in self._zsets:
+                return SortedSet(_key)
+            elif key in self._hashes:
+                return get_hash(_key)
+            return get_redis().get(_key) or ''
 
-    def get(self, key):
-        key = "%s.%s" % (self.prefix, key)
-        return get_redis.get(_key)
+    def __setattr__(self, name, value):
+        if name in ['prefix', 'id']:
+            super(Model, self).__setattr__(name, value)
+        else:
+            get_redis().set("%s.%s" % (self.prefix, name), value)
 
     def rel(self, key):
         return "%s.%s" % (self.prefix, key)
 
     def obj(self):
         '''returns a hash object'''
-        return get_hash(self.info)
+        return get_hash(self.rel('info'))
 
 
     __metaclass__ = Meta
