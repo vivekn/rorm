75ce9d3ab30991673165442bcabbf75a3a67e27d
Vivek Narayanan <mail@vivekn.co.cc>
2011-10-05 08:29:50 +0530

    Symlinks feature added

--- rorm/__init__.py
+++ rorm/__init__.py
@@ -1,5 +1,17 @@
 from redis_wrap import *
 
+GLOBAL_LINK = "__aliastable__" #Stores links between different models
+
+def create_link(from_key, to_key):
+    """
+    Creates a symlink between two keys when using the models API.
+    Note: This requires the subclassing of Model, a simple get_redis() call won't work as intended.
+    """
+    table = get_hash(GLOBAL_LINK)
+    table[from_key] = to_key
+
+
+
 class Field(object):
     def __init__(self, data_type):
         self.data_type = data_type
@@ -48,6 +60,16 @@ class Model(object):
     def __init__(self, prefix):
         self.prefix = '%s.%s' % (self.__class__.__name__, prefix)
         self.id = prefix
+        self.check_links()
+
+    def check_links(self):
+        #Check for symlinks
+        if self.prefix in get_hash(GLOBAL_LINK):
+            target_key = get_hash(GLOBAL_LINK)[self.prefix] or ''
+            if target_key and get_redis().exists(target_key):
+                self.prefix = target_key
+                self.id = target_key.split('.', 1)[1]
+
 
     def __getattr__(self, key):
         if key in ['prefix', 'id']:
@@ -71,12 +93,31 @@ class Model(object):
             get_redis().set("%s.%s" % (self.prefix, name), value)
 
     def rel(self, key):
+        self.check_links()
         return "%s.%s" % (self.prefix, key)
 
     def obj(self):
         '''returns a hash object'''
         return get_hash(self.rel('info'))
 
+    #Access properties from .info hash
+    def get(self, key):
+        info = get_hash(self.rel('info'))
+        return info.get(key, '')
+
+    def set(self, key, value):
+        info = get_hash(self.rel('info'))
+        info[key] = value
+
+    def delete(self, key):
+        info = get_hash(self.rel('info'))
+        del info[key]
+
+    def get_permalink(self):
+        return self.__class__.__name__.lower() + '/' + self.id + '/'
+
+    def exists(self):
+        return get_redis().exists(self.prefix)
 
     __metaclass__ = Meta
 
